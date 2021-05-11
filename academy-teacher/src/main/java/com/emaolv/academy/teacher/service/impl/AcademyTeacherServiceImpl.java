12124b9283ac89b4c13a3e68aaf664e375703719
cavomv <cavomv@gmail.com>
2021-05-11 14:49:02 +0800

    讲师 服务实现类

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/service/impl/AcademyTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.emaolv.academy.teacher.service.impl;
+
+import com.emaolv.academy.teacher.entity.AcademyTeacher;
+import com.emaolv.academy.teacher.mapper.AcademyTeacherMapper;
+import com.emaolv.academy.teacher.service.AcademyTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-11
+ */
+@Service
+public class AcademyTeacherServiceImpl extends ServiceImpl<AcademyTeacherMapper, AcademyTeacher> implements AcademyTeacherService {
+
+}
