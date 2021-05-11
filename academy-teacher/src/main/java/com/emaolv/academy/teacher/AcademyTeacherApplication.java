7e7ed6cb6e90912933ee398522940dd79c68ce7f
cavomv <cavomv@gmail.com>
2021-05-11 14:45:02 +0800

    AcademyTeacher启动类

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/AcademyTeacherApplication.java
@@ -0,0 +1,17 @@
+package com.emaolv.academy.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.jvmstat.perfdata.monitor.PerfStringVariableMonitor;
+
+/**
+ * @author: liu jia
+ * @description: AcademyTeacher启动类
+ * @date: Created in 2021/5/11 10:48
+ */
+@SpringBootApplication
+public class AcademyTeacherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyTeacherApplication.class,args);
+    }
+}
