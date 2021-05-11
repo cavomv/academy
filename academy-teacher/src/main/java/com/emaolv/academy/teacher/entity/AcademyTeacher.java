a5570765a0ce471fe49ede52f66e6e7a5962769e
cavomv <cavomv@gmail.com>
2021-05-11 14:42:28 +0800

    增加@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/entity/AcademyTeacher.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/entity/AcademyTeacher.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -21,8 +23,8 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@ApiModel(value="EduTeacher对象", description="讲师")
-public class EduTeacher implements Serializable {
+@ApiModel(value="AcademyTeacher对象", description="讲师")
+public class AcademyTeacher implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -53,9 +55,11 @@ public class EduTeacher implements Serializable {
     private Boolean deleted;
 
     @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private LocalDateTime gmtCreate;
 
     @ApiModelProperty(value = "更新时间")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private LocalDateTime gmtModified;
 
 
