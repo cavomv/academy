0d4c7d0d316f99b609974525e7ba5cc5e6bfdde8
cavomv <cavomv@gmail.com>
2021-05-11 18:39:50 +0800

    使用统一返回结果

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyTeacherController.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyTeacherController.java
@@ -1,6 +1,7 @@
 package com.emaolv.academy.teacher.controller;
 
 
+import com.emaolv.academy.common.result.R;
 import com.emaolv.academy.teacher.entity.AcademyTeacher;
 import com.emaolv.academy.teacher.mapper.AcademyTeacherMapper;
 import com.emaolv.academy.teacher.service.AcademyTeacherService;
@@ -34,18 +35,24 @@ public class AcademyTeacherController {
 
     @ApiOperation(value="获取所有讲师列表")
     @GetMapping("findAll")
-    public List<AcademyTeacher> findAllTeacher(){
+    public R findAllTeacher(){
 //        调用service中的方法查询所有的操作
         List<AcademyTeacher> list = academyTeacherService.list(null);
-        return list;
+        return R.success().data("list", list).message("获取列表成功");
     }
 
     @ApiOperation(value = "根据ID逻辑删除", notes = "逻辑删除数据记录")
     @DeleteMapping("{id}")
-    public boolean removeById(
+    public R removeById(
             @ApiParam(name = "id", value = "讲师ID", required = true)
             @PathVariable String id){
-        return academyTeacherService.removeById(id);
+        boolean result = academyTeacherService.removeById(id);
+        if(result){
+            return R.success().message("删除成功");
+        }
+        else{
+            return R.fail().message("删除失败");
+        }
     }
 }
 
