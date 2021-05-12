14f59b71c39657093c96477f025c02b4ec9aa60c
cavomv <cavomv@gmail.com>
2021-05-12 09:10:58 +0800

    实现MeatObjectHandler中的insertFill 和 updateFill

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/handler/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.emaolv.academy.teacher.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author: liu jia
+ * @description: 自动添加创建时间
+ * @date: Created in 2021/5/11 20:51
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
