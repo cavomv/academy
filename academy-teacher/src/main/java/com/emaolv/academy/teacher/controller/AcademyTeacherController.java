ee75680e7b840ad82c2097a33a56ff328629dbbe
cavomv <cavomv@gmail.com>
2021-05-21 15:44:28 +0800

    增加讲师管理tags

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyTeacherController.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyTeacherController.java
@@ -1,11 +1,8 @@
 package com.emaolv.academy.teacher.controller;
 
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.emaolv.academy.common.exception.CustomizeException;
 import com.emaolv.academy.common.result.R;
-import com.emaolv.academy.common.result.ResponseEnum;
 import com.emaolv.academy.teacher.entity.AcademyTeacher;
 import com.emaolv.academy.teacher.entity.vo.AcademyTeacherQuery;
 import com.emaolv.academy.teacher.service.AcademyTeacherService;
@@ -13,10 +10,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+
 
 
 import java.util.List;
@@ -30,9 +28,9 @@ import java.util.List;
  * @since 2021-05-11
  */
 @EnableOpenApi
-@Api(description = "讲师管理")
+@Api(tags = "讲师管理")
 @RestController
-@RequestMapping("/admin/core/academyTeacher")
+@RequestMapping( "/teacher")
 @Slf4j
 @CrossOrigin
 public class AcademyTeacherController {
@@ -42,39 +40,47 @@ public class AcademyTeacherController {
     private AcademyTeacherService academyTeacherService;
 
     @ApiOperation(value="获取所有讲师列表")
-    @GetMapping("list")
-    public R findAllTeacher(){
-//        调用service中的方法查询所有的操作
-        List<AcademyTeacher> teacherList = academyTeacherService.list(null);
-        return R.success().data("teacherList", teacherList).message("获取列表成功");
+    @GetMapping(value = "/list")
+    public R getTeacherList(){
+        try {
+            // 调用service中的方法查询所有的操作
+            List<AcademyTeacher> list = academyTeacherService.list(null);
+            return R.success().data("items", list).message("获取列表成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return R.fail();
+        }
     }
 
     @ApiOperation(value = "根据ID逻辑删除", notes = "逻辑删除数据记录")
     @DeleteMapping("{id}")
     public R removeById(
             @ApiParam(name = "id", value = "讲师ID", required = true)
-            @PathVariable String id){
-        boolean result = academyTeacherService.removeById(id);
-        if(result){
+            @PathVariable String id) {
+        try{
+            academyTeacherService.removeById(id);
             return R.success().message("删除成功");
-        }
-        else {
-            // 重复删除记录会提示"该记录已删除" - 自定义异常
-            throw new CustomizeException(ResponseEnum.REMOVE_ERROR);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return R.fail().message("删除失败");
         }
     }
 
+    /**
+     * 新增讲师
+     * @param academyTeacher
+     * @return
+     */
     @ApiOperation(value = "新建讲师")
     @PostMapping("/save")
     public R save(
-            @ApiParam(value = "讲师对象", required = true)
-            @RequestBody AcademyTeacher academyTeacher){
-
-        boolean result = academyTeacherService.save(academyTeacher);
-        if(result){
+            @ApiParam(name = "teacher", value = "讲师对象", required = true)
+            @RequestBody AcademyTeacher academyTeacher) {
+        try{
+            academyTeacherService.save(academyTeacher);
             return R.success().message("保存成功");
-        }
-        else{
+        } catch (Exception e) {
+            e.printStackTrace();
             return R.fail().message("保存失败");
         }
     }
@@ -98,73 +104,96 @@ public class AcademyTeacherController {
         return R.success().data("total", total).data("rows", records);
     }
 
-    @ApiOperation(value = "条件查询带分页的方法")
-    @GetMapping("pageWithCondition/{current}/{size}")
-    public R pageListTeacherWithCondition(
-            @ApiParam(value = "当前页", required = true)
-            @PathVariable Long current,
-            @ApiParam(value ="每页记录数", required = true)
-            @PathVariable Long size,
-            AcademyTeacherQuery academyTeacherQuery
-    )
-    {
+    /**
+     * 根据讲师条件分页查询
+     * @param current
+     * @param size
+     * @param academyTeacherQuery
+     * @return
+     */
+    @ApiOperation(value = "根据讲师条件分页查询")
+    @RequestMapping(value="/{current}/{size}")
+    public R selectTeacherByPage(
+            @ApiParam(name = "current", value = "当前页", required = true) @PathVariable(value = "current") long  current,
+            @ApiParam(name = "size", value = "当前页", required = true) @PathVariable(value = "size") long  size,
+            @RequestBody(required = false) AcademyTeacherQuery academyTeacherQuery) {
+               // 构建page对象
+                Page<AcademyTeacher> pageTeacher = new Page<>(current, size);
+                // 构建条件
+                QueryWrapper<AcademyTeacher> queryWrapper = new QueryWrapper<>();
+                // ASC表示升序排序，DESC表示降序排序
+                queryWrapper.orderByDesc("create_time");
+                if(academyTeacherQuery == null){
+                    academyTeacherService.page(pageTeacher, null);
+                    // 总记录数
+                    long total = pageTeacher.getTotal();
+                    // 每页记录数
+                    List<AcademyTeacher> records = pageTeacher.getRecords();
+                    return R.success().data("total", total).data("rows", records);
+                }
+                // 获取对象属性
+                String name = academyTeacherQuery.getName();
+                System.out.println("academyTeacherQuery.getName();"+academyTeacherQuery.getName());
+                Integer level = academyTeacherQuery.getLevel();
+                String begin = academyTeacherQuery.getBegin();
+                String end = academyTeacherQuery.getEnd();
+                // 判断条件值是否为空 如果不为空 则拼接条件
+                if (StringUtils.hasText(name)) {
+                    queryWrapper.like("name", name);
+                }
+                if ((String.valueOf(level) == null)){
+                    System.out.println((String.valueOf(level) == null));
+                    if(StringUtils.hasText(String.valueOf(level))){
+                        queryWrapper.eq("level", String.valueOf(level));
+                    }
+                }
 
-        // 创建page对象
-        Page<AcademyTeacher> pageListTeacherWithCondition = new Page<>(current, size);
-        // 构建条件
-        QueryWrapper<AcademyTeacher> queryWrapper = new QueryWrapper<>();
-
-        // 查询的条件
-        String name = academyTeacherQuery.getName();
-        Integer level = academyTeacherQuery.getLevel();
-        String begin = academyTeacherQuery.getBegin();
-        String end = academyTeacherQuery.getEnd();
-        // 判断条件值是否为空 如果不为可空拼接条件
-        if(!StringUtils.isEmpty(name)){
-            // 拼接条件
-            queryWrapper.like("name",name);
-        }
-        if(!(level == null)){
-            queryWrapper.like("level",level);
-        }
-        if(!StringUtils.isEmpty(begin)){
-            // 拼接条件
-            queryWrapper.ge("create_time",begin);
-        }
-        if(!StringUtils.isEmpty(end)){
-            // 拼接条件
-            queryWrapper.le("update_time",end);
-        }
-        // 实现条件查询分页功能
-        academyTeacherService.page(pageListTeacherWithCondition, queryWrapper);
-        // 总记录数
-        long total = pageListTeacherWithCondition.getTotal();
-        // 每页记录数
-        List<AcademyTeacher> records = pageListTeacherWithCondition.getRecords();
-        return R.success().data("total", total).data("rows", records);
+                if(StringUtils.hasText(begin)){
+                    // ge 大于等于 >=
+                    queryWrapper.ge("create_time",begin);
+                }
+                if(StringUtils.hasText(end)){
+                    // 小于等于 <=
+                    queryWrapper.le("update_time",end);
+                }
+                academyTeacherService.page(pageTeacher, queryWrapper);
+                long total = pageTeacher.getTotal();
+                List<AcademyTeacher> records = pageTeacher.getRecords();
+                return R.success().data("total", total).data("rows", records);
     }
 
+    /**
+     * 根据ID 查询讲师
+     * @param id
+     * @return
+     */
     @ApiOperation(value ="根据讲师ID进行查询")
-    @GetMapping("getInfo/{id}")
+    @GetMapping("{id}")
     public R getById(
-            @ApiParam(value = "讲师ID")
-            @PathVariable String id){
-        AcademyTeacher academyTeacher = academyTeacherService.getById(id);
-        return R.success().data("teacher",academyTeacher);
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id) {
+        try{
+            AcademyTeacher academyTeacher= academyTeacherService.getById(id);
+            return R.success().data("teacher", academyTeacher);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return R.fail();
+        }
     }
 
-    @ApiOperation(value ="修改讲师")
-    @PostMapping("/update")
+    @ApiOperation(value ="根据讲师ID修改讲师信息")
+    @RequestMapping("/update")
     public R update(
-            @ApiParam(value = "讲师")
+            @ApiParam(name = "teacher", value = "讲师", required = true)
             @RequestBody AcademyTeacher academyTeacher)
     {
-        boolean flag = academyTeacherService.updateById(academyTeacher);
-        if(flag){
+        try{
+            academyTeacherService.updateById(academyTeacher);
             return R.success().message("更新成功");
+        } catch (Exception e){
+            e.printStackTrace();
+            return R.fail().message("更新失败");
         }
-        return R.fail().message("更新失败");
     }
-
 }
 
