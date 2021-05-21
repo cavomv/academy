2635e60d0e33f8f542bbe303a51dd5e3e2af660b
cavomv <cavomv@gmail.com>
2021-05-21 21:25:00 +0800

    实现课程分类嵌套

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyCourseTypeController.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyCourseTypeController.java
@@ -2,13 +2,17 @@ package com.emaolv.academy.teacher.controller;
 
 
 import com.emaolv.academy.common.result.R;
+import com.emaolv.academy.teacher.entity.vo.CourseCategoryQuery;
 import com.emaolv.academy.teacher.service.AcademyCourseTypeService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -21,19 +25,27 @@ import org.springframework.web.multipart.MultipartFile;
 @Api(tags="课程分类管理")
 @CrossOrigin
 @RestController
-@RequestMapping("/academyCourseType")
+@RequestMapping("/teacher")
 public class AcademyCourseTypeController {
 
     @Autowired
     private AcademyCourseTypeService academyCourseTypeService;
 
     // 添加课程分类
-    @PostMapping("import")
+    @ApiOperation("Excel批量导入课程分类")
+    @PostMapping("/addsubject")
     public R addCourseType(
             @ApiParam(value = "文件", required = true) @RequestPart("file") MultipartFile file) {
         // 获取上传Excel文件
        academyCourseTypeService.saveCourseType(file, academyCourseTypeService);
         return R.success();
+    }
+
+    @ApiOperation("嵌套数据列表")
+    @GetMapping("/nestedlist")
+    public R nestedList(){
+        List<CourseCategoryQuery> CourseCategoryQueryList = academyCourseTypeService.nestedList();
+        return R.success().data("items",CourseCategoryQueryList);
     }
 }
 
