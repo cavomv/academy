e2436f24a98e7b8c2260e29200bfd2d46fbd4c85
cavomv <cavomv@gmail.com>
2021-05-21 15:52:23 +0800

    增加课程分类mapper

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/mapper/AcademyCourseTypeMapper.java
@@ -0,0 +1,16 @@
+package com.emaolv.academy.teacher.mapper;
+
+import com.emaolv.academy.teacher.entity.AcademyCourseType;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 课程分类 Mapper 接口
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-20
+ */
+public interface AcademyCourseTypeMapper extends BaseMapper<AcademyCourseType> {
+
+}
