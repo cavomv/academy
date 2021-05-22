ea0f407ee6abb53ea3a9026516e97c519642cd47
cavomv <cavomv@gmail.com>
2021-05-22 12:37:17 +0800

    创建讲师查询对象

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/entity/vo/AcademyTeacherQuery.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/entity/vo/AcademyTeacherQuery.java
@@ -1,5 +1,6 @@
 package com.emaolv.academy.teacher.entity.vo;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -10,16 +11,17 @@ import java.io.Serializable;
  * @description: 讲师查询条件封装
  * @date: Created in 2021/5/12 09:13
  */
+@ApiModel("讲师查询对象")
 @Data
 public class AcademyTeacherQuery implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "讲师名称， 模糊查询")
     private String name;
-    @ApiModelProperty(value = "头衔 1 高级讲师 2 首席讲师")
+    @ApiModelProperty(value = "讲师级别")
     private Integer level;
     @ApiModelProperty(value = "查询开始时间", example = "2021-01-01 10:10:10")
-    private String begin;
+    private String joinDateBegin;
     @ApiModelProperty(value = "查询结束时间", example = "2021-12-01 10:10:10")
-    private String end;
+    private String joinDateEnd;
 }
