019f2bb070f07fc186ac6eeb086653212e76ecb9
cavomv <cavomv@gmail.com>
2021-05-23 14:58:07 +0800

    课程简介接口

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/service/AcademyCourseDescriptionService.java
@@ -0,0 +1,16 @@
+package com.emaolv.academy.teacher.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.emaolv.academy.teacher.entity.AcademyCourseDescription;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-23
+ */
+public interface AcademyCourseDescriptionService extends IService<AcademyCourseDescription> {
+
+}
