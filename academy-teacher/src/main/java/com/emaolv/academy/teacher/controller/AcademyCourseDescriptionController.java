d0e1a91371b4c775e7d2a5132e0db57c7603ff78
cavomv <cavomv@gmail.com>
2021-05-23 14:52:32 +0800

    课程简介

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/AcademyCourseDescriptionController.java
@@ -0,0 +1,23 @@
+package com.emaolv.academy.teacher.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器 课程简介
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-23
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/academyCourseDescription")
+public class AcademyCourseDescriptionController {
+
+}
+
