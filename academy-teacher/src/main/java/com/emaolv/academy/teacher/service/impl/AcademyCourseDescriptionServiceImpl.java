f9923ebe0bce3465395d7a94e311370ecf220aad
cavomv <cavomv@gmail.com>
2021-05-23 14:59:16 +0800

    课程简介接口实现

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/service/impl/AcademyCourseDescriptionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.emaolv.academy.teacher.service.impl;
+
+import com.emaolv.academy.teacher.entity.AcademyCourseDescription;
+import com.emaolv.academy.teacher.mapper.AcademyCourseDescriptionMapper;
+import com.emaolv.academy.teacher.service.AcademyCourseDescriptionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-23
+ */
+@Service
+public class AcademyCourseDescriptionServiceImpl extends ServiceImpl<AcademyCourseDescriptionMapper, AcademyCourseDescription> implements AcademyCourseDescriptionService {
+
+}
