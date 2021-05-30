c5426924edeaa9529af739d596764659303e215f
cavomv <cavomv@gmail.com>
2021-05-30 11:20:55 +0800

    AcademyCourseChapterMapper

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/mapper/AcademyCourseChapterMapper.java
@@ -0,0 +1,16 @@
+package com.emaolv.academy.teacher.mapper;
+
+import com.emaolv.academy.teacher.entity.AcademyCourseChapter;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-26
+ */
+public interface AcademyCourseChapterMapper extends BaseMapper<AcademyCourseChapter> {
+
+}
