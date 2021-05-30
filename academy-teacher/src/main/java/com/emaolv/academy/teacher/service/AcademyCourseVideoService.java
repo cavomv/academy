813fb6b1b3ce04e9b84853023ce72f9ce45ac68b
cavomv <cavomv@gmail.com>
2021-05-30 11:17:43 +0800

    course video service

---
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/service/AcademyCourseVideoService.java
@@ -0,0 +1,15 @@
+package com.emaolv.academy.teacher.service;
+
+import com.emaolv.academy.teacher.entity.AcademyCourseVideo;
+import com.baomidou.mybatisplus.extension.service.IService;
+//import com.netflix.client.ClientException;
+
+/**
+ * <p>
+ * 课程视频 服务类
+ * </p>
+ *
+ * @author Jia
+ * @since 2021-05-26
+ */
+public interface AcademyCourseVideoService extends IService<AcademyCourseVideo> { }
