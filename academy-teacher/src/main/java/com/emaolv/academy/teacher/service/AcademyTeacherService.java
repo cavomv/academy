3024c091a448034c5335ff653960a1ef6234fb07
cavomv <cavomv@gmail.com>
2021-06-05 22:46:03 +0800

    增加selectTeacherInfoById接口

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/service/AcademyTeacherService.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/service/AcademyTeacherService.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.emaolv.academy.teacher.entity.AcademyTeacher;
 import com.emaolv.academy.teacher.entity.vo.AcademyTeacherQuery;
 
+import java.util.Map;
+
 /**
  * <p>
  * 讲师 服务类
@@ -18,5 +20,19 @@ public interface AcademyTeacherService extends IService<AcademyTeacher> {
 
     IPage<AcademyTeacher> selectPage(Page<AcademyTeacher> pageTeacher, AcademyTeacherQuery academyTeacherQuery);
     // 根据id 删除讲师头像
+
+    /**
+     * 删除讲师头像
+     * @param id
+     * @return
+     */
     boolean removeAvatarById(String id);
+
+    /**
+     * 讲师分页查询接口
+     * @param id 讲师ID
+     * @return 讲师信息
+     */
+    Map<String, Object> selectTeacherInfoById(String id);
+
 }
