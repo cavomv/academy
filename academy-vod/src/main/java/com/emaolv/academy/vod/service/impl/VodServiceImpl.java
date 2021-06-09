1f7b0e70223881c8ccea005f1b58d40ccb63dd8c
cavomv <cavomv@gmail.com>
2021-06-09 09:27:49 +0800

    增加视频上传业务接口实现

--- academy-vod/src/main/java/com/emaolv/academy/vod/service/impl/VodServiceImpl.java
+++ academy-vod/src/main/java/com/emaolv/academy/vod/service/impl/VodServiceImpl.java
@@ -5,8 +5,9 @@ import com.aliyun.vod.upload.req.UploadStreamRequest;
 import com.aliyun.vod.upload.resp.UploadStreamResponse;
 import com.emaolv.academy.common.exception.CustomizeException;
 import com.emaolv.academy.common.result.ResponseEnum;
-import com.emaolv.academy.vod.service.VideoService;
+import com.emaolv.academy.vod.service.VodService;
 import com.emaolv.academy.vod.utils.VodProperties;
+import io.netty.util.internal.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,31 +17,33 @@ import java.io.InputStream;
 
 /**
  * @author: liu jia
- * @description: 实现服务类
- * @date: Created in 2021/5/28 08:17
+ * @description: 实现视频业务上传
+ * @date: Created in 2021/6/8 11:10
  */
 @Service
 @Slf4j
-public class VideoServiceImpl implements VideoService {
+public class VodServiceImpl implements VodService {
 
     @Autowired
     private VodProperties vodProperties;
+
     @Override
-    public String uploadVideo(InputStream file, String originalFilename) {
+    public String uploadVideo(InputStream inputStream, String originalFilename) {
+
+        // 获取文件名
         String title = originalFilename.substring(0, originalFilename.lastIndexOf("."));
         UploadStreamRequest request = new UploadStreamRequest(
-        vodProperties.getKeyid(),
-        vodProperties.getKeysecret(),
-        title, originalFilename, file);
-        UploadVideoImpl uploader = new UploadVideoImpl();
-        UploadStreamResponse response = uploader.uploadStream(request);
+                vodProperties.getKeyid(),
+                vodProperties.getKeysecret(),
+                title, originalFilename, inputStream);
+        UploadVideoImpl uploadVideo = new UploadVideoImpl();
+        UploadStreamResponse response = uploadVideo.uploadStream(request);
         String videoId = response.getVideoId();
-        //没有正确的返回videoid则说明上传失败
-        if(StringUtils.isEmpty(videoId)){
+        // 没有正确的返回
+        if(StringUtils.isEmpty(videoId)) {
             log.error("阿里云上传失败：" + response.getCode() + " - " + response.getMessage());
             throw new CustomizeException(ResponseEnum.VIDEO_UPLOAD_ALIYUN_ERROR);
         }
         return videoId;
     }
-
 }
