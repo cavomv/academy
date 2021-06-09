b06d053678dfb48eaef2606ee8737794c1c52e3f
cavomv <cavomv@gmail.com>
2021-06-09 09:29:00 +0800

    增加vod启动类

--- academy-vod/src/main/java/com/emaolv/academy/vod/VodApplication.java
+++ academy-vod/src/main/java/com/emaolv/academy/vod/VodApplication.java
@@ -5,19 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author: liu jia
- * @description: 视频点播启动类
- * @date: Created in 2021/5/28 08:05
+ * @description:
+ * @date: Created in 2021/6/7 14:56
  */
-@ComponentScan({"com.emaolv"})
-// 取消数据源自动配置
+@EnableOpenApi
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan({"com.emaolv.academy"})
 @EnableDiscoveryClient
 public class VodApplication {
     public static void main(String[] args) {
         SpringApplication.run(VodApplication.class, args);
     }
-
 }
