fcf27a73e1ccc5cf73e53f600673fb820c537fcc
cavomv <cavomv@gmail.com>
2021-06-09 09:28:21 +0800

    创建常量读取工具类

--- academy-vod/src/main/java/com/emaolv/academy/vod/utils/VodProperties.java
+++ academy-vod/src/main/java/com/emaolv/academy/vod/utils/VodProperties.java
@@ -2,19 +2,19 @@ package com.emaolv.academy.vod.utils;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 /**
  * @author: liu jia
  * @description: 创建常量读取工具类
- * @date: Created in 2021/5/28 08:12
+ * @date: Created in 2021/6/8 10:59
  */
 
 @Data
 @Component
-@ConfigurationProperties(prefix="aliyun.vod")
+@ConfigurationProperties(prefix = "aliyun.vod")
 public class VodProperties {
-
     private String keyid;
     private String keysecret;
 }
