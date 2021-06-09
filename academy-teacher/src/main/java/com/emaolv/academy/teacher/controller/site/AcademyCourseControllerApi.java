44c9afaa719a1b048a87d9f80908d1211a92152f
cavomv <cavomv@gmail.com>
2021-06-09 09:32:58 +0800

    增加网站课程管理应用接口

--- academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/site/AcademyCourseControllerApi.java
+++ academy-teacher/src/main/java/com/emaolv/academy/teacher/controller/site/AcademyCourseControllerApi.java
@@ -2,17 +2,18 @@ package com.emaolv.academy.teacher.controller.site;
 
 import com.emaolv.academy.common.result.R;
 import com.emaolv.academy.teacher.entity.AcademyCourse;
+import com.emaolv.academy.teacher.entity.AcademyCourseChapter;
+import com.emaolv.academy.teacher.entity.vo.ChapterVo;
 import com.emaolv.academy.teacher.entity.vo.WebCourseQueryVo;
+import com.emaolv.academy.teacher.entity.vo.WebCourseVo;
+import com.emaolv.academy.teacher.service.AcademyCourseChapterService;
 import com.emaolv.academy.teacher.service.AcademyCourseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +23,7 @@ import java.util.List;
  * @date: Created in 2021/6/5 21:25
  */
 @RestController
-@Api(tags="网站课程")
+@Api(tags="网站课程管理")
 @CrossOrigin
 @Slf4j
 @RequestMapping("api/v1/course")
@@ -30,6 +31,8 @@ public class AcademyCourseControllerApi {
 
     @Autowired
     private AcademyCourseService academyCourseService;
+    @Autowired
+    private AcademyCourseChapterService academyCourseChapterService;
 
     @ApiOperation("课程列表")
     @GetMapping("pageList")
@@ -38,6 +41,22 @@ public class AcademyCourseControllerApi {
             WebCourseQueryVo webCourseQueryVo){
         List<AcademyCourse> academyCourseList = academyCourseService.webPageList(webCourseQueryVo);
         return R.success().data("courseList", academyCourseList);
+
+    }
+
+    @ApiOperation("根据ID查询课程信息详情")
+    @GetMapping("get/{courseId}")
+    public R getById(
+            @ApiParam(value = "课程ID", required = true)
+                    @PathVariable(value = "courseId") String courseId){
+        // 查询课程信息和讲师信息
+        WebCourseVo webCourseVo = academyCourseService.selectWebCourseVoById(courseId);
+
+        // 查询章节嵌套信息
+        List<ChapterVo> chapterVoList = academyCourseChapterService.nestedList(courseId);
+
+        return R.success().data("course", webCourseVo).data("chapterVoList", chapterVoList);
+
 
     }
 
